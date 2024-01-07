93f6fb97298d990c20abd1b4755e23a6a845c773
tallereaml <155858537+tallereaml@users.noreply.github.com>
2024-01-07 17:37:10 -0600

    Delete autenticacion.js

--- autenticacion.js
+++
@@ -1,7 +0,0 @@
-import {GoogleAuthPrvider,signInWithRedirect} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-GoogleAuthProvider.js";
-
-//import {GoogleAuthProvider, signInWithRedirect} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-
-var provider = new firebase.auth.GoogleAuthProvider();
-
-firebase.auth().signInWithRedirect(provider);
