09fa7df4826551d7b302907c8c65d8d73d6527ab
tallereaml <155858537+tallereaml@users.noreply.github.com>
2024-01-08 18:23:06 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-import './scripts/inicioFirebase.js'
-import './scripts/autenticacion.js'
-import './scripts/github.js'
-import './scripts/google.js'
+    import './scripts/inicioFirebase.js'
+    import './scripts/autenticacion.js'
+    import './scripts/github.js'
+    import './scripts/google.js'
+    
 
